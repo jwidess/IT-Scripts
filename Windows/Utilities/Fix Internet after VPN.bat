2ccbb1120fa3e301d56e31a73aa068c12d0c0c40
ht-jwiden <jwiden@hi-techniques.com>
2024-12-18 11:26:42 -0600

    Create Fix Internet after VPN.bat

---
+++ Windows/Utilities/Fix Internet after VPN.bat
@@ -0,0 +1,45 @@
+@echo off
+title Fixing the interwebs...
+color 09
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------    
+
+@echo on
+
+echo ------- This will delete route 0.0.0.0 then renew
+TIMEOUT 1
+route delete 0.0.0.0
+TIMEOUT 2
+ipconfig /renew
+TIMEOUT 1
+ipconfig /flushdns
+TIMEOUT 1
+ping google.com
+TIMEOUT 2
