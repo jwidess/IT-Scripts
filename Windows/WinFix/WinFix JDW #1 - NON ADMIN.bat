a7feb7010b0f2a9c4c65cf54e7d2194fd97bebb7
ht-jwiden <jwiden@hi-techniques.com>
2025-01-21 10:32:38 -0600

    Update WinFix JDW #1 - NON ADMIN.bat

--- Windows/WinFix/WinFix JDW #1 - NON ADMIN.bat
+++ Windows/WinFix/WinFix JDW #1 - NON ADMIN.bat
@@ -21,9 +21,10 @@ cls
 echo -------------------------------------------------
 echo Checking the Windows partition - procedure 1 of 4
 echo -------------------------------------------------
+echo Running: chkdsk c: /scan
 chkdsk c: /scan
 echo -------------------------------------------
-echo If it finds some problems, run chkdsk c: /f
+echo If chkdsk finds problems, run chkdsk c: /f
 echo -------------------------------------------
 echo --------------------------
 echo PRESS ANY KEY TO CONTINUE.
@@ -32,10 +33,12 @@ C:\windows\system32\timeout 5
 echo ------------------------------------------------
 echo Windows component files check - procedure 2 of 4
 echo ------------------------------------------------
+echo Running: Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 echo --------------------------------------------------
 echo Phase 1 of 2 completed
 echo --------------------------------------------------
+echo Running: Dism.exe /online /Cleanup-Image /SPSuperseded
 Dism.exe /online /Cleanup-Image /SPSuperseded
 echo --------------------------------------------------
 echo Phase 2 of 2 completed
@@ -46,14 +49,17 @@ C:\windows\system32\timeout 5
 echo --------------------------------------------------------------
 echo Checking the integrity of the Windows image - procedure 3 of 4
 echo --------------------------------------------------------------
+echo Running: DISM /Online /Cleanup-Image /CheckHealth
 DISM /Online /Cleanup-Image /CheckHealth
 echo --------------------------------------------------
 echo Phase 1 of 3 completed
 echo --------------------------------------------------
+echo Running: DISM /Online /Cleanup-Image /ScanHealth
 DISM /Online /Cleanup-Image /ScanHealth
 echo --------------------------------------------------
 echo Phase 2 of 3 completed
 echo --------------------------------------------------
+echo Running: DISM /Online /Cleanup-Image /RestoreHealth
 DISM /Online /Cleanup-Image /RestoreHealth
 echo --------------------------------------------------
 echo Phase 3 of 3 completed 
@@ -64,6 +70,7 @@ C:\windows\system32\timeout 5
 echo -------------------------------------------------
 echo Running System file check - procedure 4 of 4
 echo -------------------------------------------------
+echo Running: sfc /scannow
 sfc /scannow
 echo --------------------------------------------------------------------------------
 echo If SFC found some errors and could not repair, re-run the script after a reboot.
